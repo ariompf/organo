8a100c2cc949d40ed6fa29c94b87614e89e6d868
Ariom Pimenta <ariompf@gmail.com>
2024-06-19 06:40:45 -0300

    add form

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,9 +1,12 @@
 import Banner from './componentes/Banner/Banner.js';
+import Formulario from './componentes/Formulario/index.js';
+
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <Formulario />
     </div>
   );
 }
