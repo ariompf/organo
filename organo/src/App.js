8580dc185db88f0db23b44beeba43316e5125372
Ariom Pimenta <ariompf@gmail.com>
2024-06-19 05:11:26 -0300

    excluir arquivos

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,26 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import Banner from './componentes/Banner/Banner.js';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Hello World</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
